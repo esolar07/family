32e1b596f68f28d781932c7e27a0a91007b67bdd
esolar07 <esolar07@gmail.com>
2016-11-15 17:59:34 -0500

    made more changes to layout of page

--- index.php
+++ index.php
@@ -1,13 +1,37 @@
 
 <?php 
-	$pgTitle = "Home";
-	
+	$pgTitle = "Home";	
 	include ("inc/header.php");
 ?>
 
-
-        <!-- Add your site or application content here -->
-        <p>Hello world! This is HTML5 Boilerplate.</p>
-		
-		
+	<section class="jumbotron">
+		<div class="familyPic">
+			<img class="familyPic__lrgImg" alt="Solar Family Image" src="http://sachagoldberger.com/portfolio/wp-content/uploads/15_07_13_Super-H%C3%A9ros-Flamands-_Triplette_028-C-copie.jpg">
+		</div>
+	</section>
+	<section class="familyMembers">
+		<div class="familyMembers__row familyMembers--1">
+			<div class="familyMembers__parent">
+				<img src="">
+			</div>
+			<div class="familyMembers__parent">
+				<img src="">
+			</div>
+		</div>
+		<div class="familyMembers__row familyMembers--2">
+			<div class="familyMembers__kid">
+				<img src="">
+			</div>
+			<div class="familyMembers__kid">
+				<img src="">
+			</div>
+			<div class="familyMembers__kid">
+				<img src="">
+			</div>
+			<div class="familyMembers__kid">
+				<img src="">
+			</div>
+		</div>
+	</section>
+	
 <?php include ("inc/footer.php");?>
